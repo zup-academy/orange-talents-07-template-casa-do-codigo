3961ac2384160e16de149d70b2fd3d4096de5e91
Rick Camelo Martins <rick.martins@zup.com.br>
2021-07-23 17:24:53 -0300

    primeiro commit

---
+++ src/test/java/orange/talents/rick/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.talents.rick.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
