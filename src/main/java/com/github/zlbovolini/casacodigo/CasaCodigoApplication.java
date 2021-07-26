fac7ab6ace096330320bc8ad6f712df2361bfea5
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-07-26 09:42:05 -0300

    Adicionado configuracoes inicias

---
+++ src/main/java/com/github/zlbovolini/casacodigo/CasaCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.github.zlbovolini.casacodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaCodigoApplication.class, args);
+	}
+
+}
